1133ca86e9e50cc90e05d9f80fd3b668314b292a
Ramaiah Arunachalam <ar.ramaiah@gmail.com>
2023-02-04 19:59:24 +0530

    Update and rename HasARealationship.java to LearnInterface.java

--- LearnInterface.java
+++ LearnInterface.java
@@ -1,6 +1,6 @@
 package Concepts;
 
-public class HasARealationship {
+public class LearnInterface {
 	public static void main(String[] args) {
 		A2BIndiaa b = new A2BIndiaa();
 		b.menu();
@@ -12,6 +12,7 @@ interface A2Bb {
 	public  void tax();
 	public  void menu();
 }
+
 class A2BIndiaa implements A2Bb {
 public	void tax() {
 		System.out.println("5");
@@ -20,6 +21,7 @@ public	void menu() {
 		System.out.println("India menu");
 	}
 }
+
 class A2BUSs implements A2Bb {
 	public	void tax() {
 		System.out.println("10");
