28933cfee05947a86491b3bcba92089831e22a55
Ramaiah Arunachalam <ar.ramaiah@gmail.com>
2023-02-27 00:37:38 +0530

    Add files via upload

---
+++ PrintJava.java
@@ -0,0 +1,13 @@
+package MyArray;
+
+public class PrintJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("JJJJJ     A     V       V    A");
+		System.out.println("  J      A A     V     V    A A");
+		System.out.println("  J     A   A     V   V    A   A");
+		System.out.println("  J    A A A A     V V    A A A A");
+		System.out.println(" JJ   A       A     V    A       A");
+	}
+}
